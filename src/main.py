409d4333b71e4eaeabc35839435ae40b3a20e2d9
Ruslan Kolomiets <rk31@yandex.ru>
2024-11-12 23:56:03 +0300

    fix main for use widget

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-from masks import get_mask_card_number, get_mask_account
+# from datetime import datetime
+from src.widget import get_date, mask_account_card
 
-user_input_card_number = input('''Введите номер карты
-- ''')
-print(get_mask_card_number(user_input_card_number))
+# from masks import get_mask_card_number, get_mask_account
 
-user_input_account_number = input(''' Введите номер счета
--''')
-print(get_mask_account(user_input_account_number))
+# user_input_card_number = input('''Введите номер карты
+# - ''')
+# print(get_mask_card_number(user_input_card_number))
+#
+# user_input_account_number = input(''' Введите номер счета
+# -''')
+# print(get_mask_account(user_input_account_number))
 
-
-
+user_input_account_card_number = input("Введите номер счета или карты")
+print(mask_account_card(user_input_account_card_number))
+print(get_date("2024-03-11T02:26:18.671407"))
