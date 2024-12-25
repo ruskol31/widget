fd29e7966d2fbbc3fc73ee28273dbbdb5c5bf844
Ruskol31 <rk31@yandex.ru>
2024-12-25 12:39:48 +0300

    создание декоратора написание теста к нему

--- src/decorators.py
+++ src/decorators.py
@@ -8,45 +8,40 @@
 
 import functools
 import logging
+from typing import Any
 
-def log(filename=None):
-    # Настройка логирования
+
+def log(filename: str = "mylog.txt") -> Any:
+    """Ожидаемый вывод в лог-файл mylog.txt my_function ok, result =
+    Ожидаемый вывод при ошибке:
+    my_function error: тип ошибки. Inputs: (1, 2), {}"""
+
     if filename:
-        logging.basicConfig(filename=filename, level=logging.INFO, format='%(asctime)s - %(message)s')
+        logging.basicConfig(filename=filename, level=logging.INFO, format="%(asctime)s - %(message)s")
     else:
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
-    def decorator(func):
+    def decorator(func: Any) -> Any:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             func_name = func.__name__
             try:
                 # logging.info(f"Начало выполнения функции: {func_name} с аргументами: {args}, {kwargs}")
                 result = func(*args, **kwargs)
-                logging.info(f"{func_name} ок")
+                logging.info(f"{func_name} ок, result ={result}")
                 return result
             except Exception as e:
                 logging.error(f"{func_name} error: тип ошибки: {type(e).__name__}. Inputs: {args}, {kwargs}")
                 raise
+
         return wrapper
+
     return decorator
 
-@log(filename= None)
-def my_function(x, y):
+
+@log(filename="mylog.txt")
+def my_function(x: int, y: int) -> int:
     return x + y
 
-my_function(1, 2)
 
-# Ожидаемый вывод в лог-файл
-# mylog.txt
-#  при успешном выполнении:
-#
-# my_function ok
-#
-# Ожидаемый вывод при ошибке:
-#
-# my_function error: тип ошибки. Inputs: (1, 2), {}
-#
-# Где
-# тип ошибки
-#  заменяется на текст ошибки.
+my_function(1, 2)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,6 +1,22 @@
-from src.decorators import log, my_function
-def test_log(capsys):
-    # @log(filename=None)
-    my_function(1, 2)
-    captured = capsys.readouterr()
-    assert captured.out == "my_function ок\n"
+import pytest
+import logging
+from src.decorators import log
+
+
+@log(filename=None)
+def my_function(x, y):
+    return x + y
+
+
+def test_my_function(caplog):
+    with caplog.at_level(logging.INFO):
+        result = my_function(1, 2)
+        assert result == 3
+        assert "my_function ок, result =3" in caplog.text
+
+
+def test_my_function(caplog):
+    with caplog.at_level(logging.ERROR):
+        with pytest.raises(TypeError):
+            my_function(1, 2, 3)
+        assert "my_function error: тип ошибки: TypeError. Inputs: (1, 2, 3), {}" in caplog.text
