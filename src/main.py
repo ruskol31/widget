1823314f3a84f2613f5bcefb2d92c9e13743289f
Ruskol31 <rk31@yandex.ru>
2025-02-27 18:54:38 +0300

    реализация программы

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # from datetime import datetime
-
+from src.processing import filter_transactions_by_description
 from src.widget import get_date, mask_account_card
 from utils import load_operations_list, read_financial_operations, read_financial_operations_exel
 
@@ -26,12 +26,52 @@ user_input_type_of_data = input(f"Выберите необходимый пун
 if user_input_type_of_data == "1":
     print("пользователь выбрал загрузку из JSON файла")
     operation_list = load_operations_list(r'C:\pytnon\widget\data\operations.json')
-    print(operation_list)
 elif user_input_type_of_data == "2":
     print("пользователь выбрал загрузку из CSV файла")
     operation_list = read_financial_operations(r'C:\pytnon\widget\data\transactions.csv')
-    print(operation_list)
 elif user_input_type_of_data == "3":
     print("пользователь выбрал загрузку из XLSX файла")
     operation_list = read_financial_operations_exel(r'C:\pytnon\widget\data\transactions_excel.xlsx')
-    print(operation_list)
+else:
+    print("Неверный выбор")
+
+# print(operation_list)
+valid_statuses = {'EXECUTED', 'CANCELED', 'PENDING'}
+while True:
+    status = input(
+        "Введите статус, по которому необходимо выполнить фильтрацию. Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n").strip().upper()
+    if status in valid_statuses:
+        break
+    print(f"Статус операции \"{status}\" недоступен.")
+
+filtered_transactions = [t for t in operation_list if t['state'].upper() == status]
+print(f"Операции отфильтрованы по статусу \"{status}\"")
+
+if not filtered_transactions:
+    print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+
+sort_choice = input("Отсортировать операции по дате? Да/Нет\n").strip().lower()
+if sort_choice == 'да':
+    order_choice = input("Отсортировать по возрастанию или по убыванию?\n").strip().lower()
+    ascending = order_choice == 'по возрастанию'
+    filtered_transactions.sort(key=lambda x: x['date'], reverse=not ascending)
+
+ruble_choice = input("Выводить только рублевые тразакции? Да/Нет\n").strip().lower()
+if ruble_choice == 'да':
+    filtered_transactions = [t for t in filtered_transactions if t['currency_code'] == 'RUB']
+
+description_choice = input(
+        "Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n").strip().lower()
+if description_choice == 'да':
+    search_string = input("Введите слово для фильтрации по описанию:\n").strip()
+    filtered_transactions = filter_transactions_by_description(filtered_transactions, search_string)
+
+if not filtered_transactions:
+    print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+else:
+    print("Распечатываю итоговый список транзакций...")
+    print(f"Всего банковских операций в выборке: {len(filtered_transactions)}")
+    for transaction in filtered_transactions:
+        print(f"{transaction['date']} {transaction['description']}")
+        print(f"{transaction['from']} -> {transaction['to']}")
+        print(f"Сумма: {transaction['amount']} {transaction['currency_code']}\n")
